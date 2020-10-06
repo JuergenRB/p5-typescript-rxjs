924ca489dd2b37e860a2ccbb773533be55fcaad9
JuergenRB <22403655+JuergenRB@users.noreply.github.com>
2020-10-06 15:19:19 +0200

    Develop (#4)
    * Added p5 and @types/p5
    
    * Added simple example - "typescripted"

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-//index.ts
+import p5 from 'p5'
+
+function sketch(p: p5) {
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  }
+
+  p.draw = () => {
+    p.background(220);
+  }
+}
+
+new p5(sketch)
